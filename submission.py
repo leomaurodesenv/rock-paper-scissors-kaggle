915a74733d0b704b7b06219bb038a0a76324188e
Leonardo Moraes <leo.mauro.desenv@gmail.com>
2020-11-09 22:42:11 -0300

    v1.0 - roulette

--- submission.py
+++ submission.py
@@ -1,20 +1,32 @@
-DEF_ENUM = {
-    'rock': 0,
-    'paper': 1,
-    'scissors': 2
-}
+## --- name to value
+DEF_ENUM = {'rock': 0, 'paper': 1, 'scissors': 2}
 
-DEF_GUESS = {
-    0: 2, # pedra -> tesoura
-    1: 0, # papel -> pedra
-    2: 1  # tesoura -> papel
-}
+## --- count number of actions
+DEF_ACTIONS = [0, 1, 2] ## possabilities
+DEF_OPPOSITE = {0: 1, 1: 2, 2: 0}
+DEF_LAST_ACTIONS = [0, 0, 0]
+
 
 import random
+from numpy.random import choice
+
 
 def nice_guess_agent(observation, configuration):
     print(observation)
     if observation.step > 0:
-        return DEF_GUESS[observation.lastOpponentAction]
+        # see the last action
+        idx = observation.lastOpponentAction
+        # increment number of plays
+        DEF_LAST_ACTIONS[idx] += 1
+        ## -- compute probability
+        tmpMax = max(DEF_LAST_ACTIONS)
+        tmpProb = [(tmpMax-i) for i in DEF_LAST_ACTIONS]
+        tmpSum = sum(tmpProb)
+        tmpProb = [round(float(i/tmpSum), 4) for i in tmpProb]
+        # guess the next action
+        actionGuess = choice(DEF_ACTIONS, 1, p=tmpProb)[0]
+        # return a nice move based on the guessing
+        return DEF_OPPOSITE[actionGuess]
     else:
+        # first move - random
         return random.randint(0, 2)
